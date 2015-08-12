ca4c11bb4ea90405f18cb7414a5f2c8b15710322
freedom1258 <qianfeng@qianfengdeMac-mini-50.local>
2015-08-12 17:45:39 +0800

    chess
    chess

---
+++ UI4_LableChess/UI4_LableChess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI4_LableChess
+//
+//  Created by qianfeng on 15/6/29.
+//  Copyright (c) 2015年 qianfeng32. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI4_LableChess/UI4_LableChess/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UI4_LableChess
+//
+//  Created by qianfeng on 15/6/29.
+//  Copyright (c) 2015年 qianfeng32. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
